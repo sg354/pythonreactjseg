6a008e5108a16ac53954bfbb063215f01bdf795f
sg354 <tech.kumar354@gmail.com>
2024-08-11 17:56:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome To Ashok IT...!!
+        </p>
+        <a
+          className="App-link"
+          href="https://pointerpointer.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         Visit To Pointing Your Pointer
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
