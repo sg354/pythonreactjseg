75742911671c42d52bc764bc8438cbf4a74a6f55
sg354 <tech.kumar354@gmail.com>
2024-08-11 23:44:55 +0530

    changing asokit to my fun msg

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome To Ashok IT...!!
+        :-)Visit To Pointing Your Pointer (*_*)...!!
         </p>
+
         <a
           className="App-link"
           href="https://pointerpointer.com/"
